52f9f01ec39a17151e20473a9604ce4d92249080
bomfimanac <bbomfim.ana@gmail.com>
2026-06-22 12:56:29 -0300

    alterações

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Toda a animação e os efeitos visuais (como o preço e o carrossel) 
+    // foram otimizados e estão sendo controlados diretamente pelo CSS.
+    console.log("Giardino Segreto - Interface carregada com sucesso!");
+});
